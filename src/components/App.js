0ef79771263c0ee12213550960b290f523945f34
Aquib Abtahi Turjo <aquib.turjo@gmail.com>
2024-06-12 06:46:29 -0400

    landing-page-commit-1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Landing1 from './landing-page/Landing1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/landing-page/Landing1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./landing1/Navbar";
+import LandingHero from './landing1/LandingHero';
+
+function Landing1() {
+    return (
+      <div className="landing1">
+        <Navbar />
+        <LandingHero />
+      </div>
+    );
+  }
+  
+  export default Landing1;
