ba9712d6d81707abd2b2074ecaef6d2b963b572c
Nayeem Mehedi <nmnaimmahadi@gmail.com>
2024-06-26 13:41:01 -0400

    self profile api

--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -3,11 +3,13 @@ import {
   createProfile,
   getProfile,
   updateProfile,
+  getSelfProfile
 } from '../controllers/profileController.js';
 
 const router = Router();
 
 router.post('/', createProfile);
+router.get('/self', getSelfProfile);
 router.get('/:id', getProfile);
 router.put('/:id', updateProfile);
 
